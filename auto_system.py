f51f9bc54552c5a5fde01ade06b39fd183a18c71
Etarnity Global <programmer.fahim.online@gmail.com>
2026-05-04 15:51:57 +0600

    Complited FIles

---
+++ auto_system.py
@@ -0,0 +1,28 @@
+import pyautogui
+import time
+import keyboard
+
+running = False
+
+def start():
+    global running
+    running = True
+    print("System Started ✅")
+
+def stop():
+    global running
+    running = False
+    print("System Stopped ❌")
+
+keyboard.on_press_key("s", lambda _: start())
+keyboard.on_press_key("q", lambda _: stop())
+
+print("Press S to START")
+print("Press Q to STOP")
+
+while True:
+    if running:
+        pyautogui.moveRel(5, 0, duration=0.2)
+        pyautogui.moveRel(-5, 0, duration=0.2)
+        pyautogui.click()
+    time.sleep(0.1)
